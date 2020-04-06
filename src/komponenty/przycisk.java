73b2e13e1e1e98ce9327f57bbc7013bceb3f5d3b
Patryk <cyganpa@gmail.com>
2020-04-07 00:53:29 +0200

    Dodano minScale oraz maxScale, a także stworzono tablice poszczególnych elementów multimetru

--- src/komponenty/przycisk.java
+++ src/komponenty/przycisk.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package komponenty;
 
+import java.awt.GridLayout;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -25,30 +21,75 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
-/**
- *
- * @author KOMPUTER
- */
 public class przycisk extends JPanel implements KeyListener,MouseWheelListener{
-    private JPanel panel;
-    private JLabel label;
+    private double min, max, roznica, scale;
+    private double maxScale, minScale;
+    private JLabel label, label1,label2,label3,label4,label5,label6,label7,label8, labelmin, labelmax;
+    private JLabel[] labele; 
     String[] images={"gora.png","prawogora.png","prawo.png","prawodol.png","dol.png","lewodol.png","lewo.png","lewogora.png"};
     private File Clap;
     private int x=0;
     public przycisk(){
         Clap=new File("E:\\Dev\\CygBacKomp\\Kompo\\src\\komponenty\\klik.wav");
-        panel=new JPanel();
         label=new JLabel();
-   
-        
+        label1= new JLabel("jajamiomate");
+        label2= new JLabel("jaja");
+        label3= new JLabel("jaja");
+        label4= new JLabel("jaja");
+        label5= new JLabel("jaja");
+        label6= new JLabel("jaja");
+        label7= new JLabel("jaja");
+        label8= new JLabel("jaja");
+        labelmin= new JLabel("min");
+        labelmax = new JLabel("max");
+        labele = new JLabel[]{label1,label2,label3,label4,label5,label6,label7,label8};
+        setLayout(new GridLayout(3, 3));
         ImageIcon iconImage = new ImageIcon(this.getClass().getResource(images[x]));
         label.setIcon(iconImage);
         addKeyListener(this);
         addMouseWheelListener(this);
         setFocusable(true);
+        
+        add(label8);
+        add(label1);
+        add(label2);
+        add(label7);
         add(label);
+        add(label3);
+        add(label6);
+        add(label5);
+        add(label4);
         setVisible(true);
+       
     }
+
+    public double getMaxScale() {
+        return maxScale;
+    }
+
+    public void setMaxScale(double maxScale) {
+        this.maxScale = maxScale;
+        labelmax.setText(String.valueOf(maxScale));
+
+    }
+
+    public double getMinScale() {
+        return minScale;
+    }
+
+    public void setMinScale(double minScale) {
+        this.minScale = minScale;
+        labelmin.setText(String.valueOf(minScale));
+        roznica = Double.parseDouble(labelmax.getText()) - Double.parseDouble(labelmin.getText());
+        scale = roznica/8;
+        for(int i=0;i<8;i++)
+        {
+            labele[i].setText(""+((i+1)*scale+ Double.parseDouble(labelmin.getText()))); 
+            
+        }
+    }
+    
+    
 
         
         
